7afd95720524f716e388a77613f37eaf4e685581
Ratika Bhuwalka <ratika.bhuwalka@sjsu.edu>
2021-10-10 13:48:08 -0700

    cors

--- server/app.js
+++ server/app.js
@@ -14,26 +14,18 @@ const cors = require('cors');
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'http://ec2-34-220-244-18.us-west-2.compute.amazonaws.com:3000', credentials: true }));
 
-// app.use(function (req, res, next) {
+app.use(function (req, res, next) {
   
-//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-//   res.setHeader('Access-Control-Allow-Credentials', 'true');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-//   res.setHeader('Cache-Control', 'no-cache');
-//   next();
-// });
+  res.Header('Access-Control-Allow-Origin', 'http://ec2-34-220-244-18.us-west-2.compute.amazonaws.com:3000');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
+});
 
-//use express session to maintain session data
-// app.use(session({
-//   secret              : 'cmpe273_kafka_passport_mongo',
-//   resave              : false, // Forces the session to be saved back to the session store, even if the session was never modified during the request
-//   saveUninitialized   : false, // Force to save uninitialized session to db. A session is uninitialized when it is new but not modified.
-//   duration            : 60 * 60 * 1000,    // Overall duration of Session : 30 minutes : 1800 seconds
-//   activeDuration      :  5 * 60 * 1000
-// }));
 
 app.use(express.static('./public'));
 
