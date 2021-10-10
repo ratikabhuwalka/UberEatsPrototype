45e4ab170d20051df3463096a3e0735fd417a9f1
Ratika Bhuwalka <ratika.bhuwalka@sjsu.edu>
2021-10-10 13:36:44 -0700

    cors

--- server/app.js
+++ server/app.js
@@ -16,15 +16,15 @@ app.use(cookieParser());
 
 app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 
-app.use(function (req, res, next) {
+// app.use(function (req, res, next) {
   
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-  res.setHeader('Cache-Control', 'no-cache');
-  next();
-});
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+//   res.setHeader('Cache-Control', 'no-cache');
+//   next();
+// });
 
 //use express session to maintain session data
 // app.use(session({
