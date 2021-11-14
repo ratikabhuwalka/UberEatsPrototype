6d095ae642bef6375e27dc693c4dd1c5d7a5b11d
Ratika Bhuwalka <ratika.bhuwalka@sjsu.edu>
2021-11-14 14:46:27 -0800

    ec2

--- client/src/webConfig.js
+++ client/src/webConfig.js
@@ -1,3 +1,3 @@
-const backendServer ="http://ec2-3-141-100-248.us-east-2.compute.amazonaws.com:3000/";
+const backendServer ="http://ec2-3-141-100-248.us-east-2.compute.amazonaws.com:3000";
 
 export default backendServer;
